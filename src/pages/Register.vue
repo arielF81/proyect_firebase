6526aa45c9d81238086c8f416b0cf2a1ced05ab9
arielF81 <104766692+arielF81@users.noreply.github.com>
2022-10-23 20:56:53 -0300

    Update Register.vue

--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -1,23 +1,59 @@
 <script>
+import {register} from "../services/auth.js";
+
+
 export default {
     name: "Register",
+    
+    data: () => ({
+        form: {
+            email: '',
+            password: '',
+        },
+        status: {
+            text: '',
+            type: 'success',
+        }
+    }),
+    methods: {
+        handleRegister() {
+            
+
+            register({...this.form})
+                .then(() => {
+                    this.status = {
+                        text: 'Cuenta creada',
+                        type: 'success',
+                    }
+                })
+                .catch(err => {
+                    this.status = {
+                        type: 'danger',
+                        
+                    }
+                });
+        }
+    }
 };
 </script>
 
 <template>
     <section>
         <h1 class="mb-3">Crear una cuenta </h1>
-
-        <form action="#" method="post">
+        <div
+            v-if="status.text !== ''"
+            :class="['alert', `alert-${status.type || 'info'}`]"
+        >{{ status.text }}</div>
+        <form action="#" method="post"  @submit.prevent="handleRegister">
             <div class="mb-3">
                 <label for="email" class="form-label">Email</label>
-                <input type="email" name="email" id="email" class="form-control">
+                <input type="email" name="email" id="email" class="form-control"  v-model="form.email">
             </div>
             <div class="mb-3">
                 <label for="password" class="form-label">Password</label>
-                <input type="password" name="password" id="password" class="form-control">
+                <input type="password" name="password" id="password" class="form-control" v-model="form.password" >
             </div>
-            <button type="submit" class="btn btn-primary w-100">Ingresar</button>
+            <button type="submit" class="btn btn-primary w-100">Crear</button>
         </form>
     </section>
 </template>
